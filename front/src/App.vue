0bb894a9323a4b6388411db1c1f0f993cf7da87c
gui555pl <gpl2@cin.ufpe.br>
2023-04-11 04:55:51 -0300

    feat: add presenca tela

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,5 +1,11 @@
 <template lang="pug">
   v-app
+    v-app-bar(app dark prominent src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg")
+      v-app-bar-nav-icon
+      v-toolbar-title Gymfit
+      v-spacer
+      v-btn(icon)
+        v-icon mdi-export
     v-main
       router-view
 </template>

29d09fd159cc00d0bdbca06fd0646c97afbaa1b0
Igor Henrique <ihsrp@cin.ufpe.br>
2023-04-13 20:36:32 -0300

    put editar treinos on routes

--- SOA/backend/treino-service/src/routes.ts
+++ SOA/backend/treino-service/src/routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { container } from "tsyringe";
 
 import TelaCadastrarTreinos from "./comunicacao/TelaCadastrarTreinos";
+import TelaEditarTreinos from "./comunicacao/TelaEditarTreinos";
 import TelaListarTreinos from "./comunicacao/TelaListarTreinos";
 
 const telaListarTreinos = container.resolve(TelaListarTreinos);
 const telaCadastrarTreinos = container.resolve(TelaCadastrarTreinos);
+const telaEditarTreinos = container.resolve(TelaEditarTreinos);
 
 const routes = Router();
 
@@ -16,5 +18,7 @@ routes.get("/treino", (req, res) =>
 routes.post("/treino", (req, res) =>
   telaCadastrarTreinos.cadastrarTreino(req, res)
 );
+
+routes.put("/treino", (req, res) => telaEditarTreinos.editarTreino(req, res));
 
 export default routes;

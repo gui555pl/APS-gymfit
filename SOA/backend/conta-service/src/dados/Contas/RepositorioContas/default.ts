c3b3c1d6ab83dea90798676ab1f41eb05f892914
Igor Henrique <ihsrp@cin.ufpe.br>
2023-04-14 14:29:10 -0300

    pass right classes

--- SOA/backend/conta-service/src/dados/Contas/RepositorioContas/default.ts
+++ SOA/backend/conta-service/src/dados/Contas/RepositorioContas/default.ts
@@ -1,10 +1,12 @@
 import Conta from "../../../negocio/Conta/Conta";
+import ContaAluno from "../../../negocio/Conta/ContaAluno";
+import ContaTreinador from "../../../negocio/Conta/ContaTreinador";
 
 const ContasDefault: Conta[] = [
-  new Conta('Guilherme', 1, 'guilherme@email.com'),
-  new Conta('Igor', 2, 'igor@email.com'),
-  new Conta('Vinícius', 3, 'vinicius@email.com'),
-  new Conta('Rogério', 4, 'rogerio@email.com'),
+  new ContaAluno("Guilherme", 1, "guilherme@email.com"),
+  new ContaAluno("Igor", 2, "igor@email.com"),
+  new ContaTreinador("Vinícius", 3, "vinicius@email.com"),
+  new ContaTreinador("Rogério", 4, "rogerio@email.com")
 ];
 
 export default ContasDefault;
